04f0af331f411573644371f7bb2084cb49aab923
Laurent Lasudry <lasudry@gmail.com>
2021-09-14 10:11:52 +0200

    Cleanup

--- src/imio/news/core/setuphandlers.py
+++ src/imio/news/core/setuphandlers.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from plone import api
 from Products.CMFPlone.interfaces import INonInstallable
 from zope.interface import implementer
-import os
 
 
 @implementer(INonInstallable)
@@ -17,21 +15,6 @@ class HiddenProfiles(object):
 
 def post_install(context):
     """Post install script"""
-    portal = api.portal.get()
-    default_entity = api.content.create(
-        type="imio.news.Entity", title="Imio", container=portal
-    )
-    faceted_config = "/faceted/config/news.xml"
-    # Create global faceted newsfolder
-    faceted = api.content.create(
-        type="imio.news.NewsFolder", title="News folder", container=default_entity
-    )
-    subtyper = faceted.restrictedTraverse("@@faceted_subtyper")
-    subtyper.enable()
-    with open(os.path.dirname(__file__) + faceted_config, "rb") as faceted_config:
-        faceted.unrestrictedTraverse("@@faceted_exportimport").import_xml(
-            import_file=faceted_config
-        )
 
 
 def uninstall(context):
