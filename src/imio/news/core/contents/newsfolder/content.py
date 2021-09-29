db9da5bc09edb7e3b6df63da0c3770e716a6a060
Christophe Boulanger <christophe.boulanger@imio.be>
2021-09-29 10:33:37 +0200

    Add field + widget to NewsFolder content type.

--- src/imio/news/core/contents/newsfolder/content.py
+++ src/imio/news/core/contents/newsfolder/content.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
+
+from imio.smartweb.locales import SmartwebMessageFactory as _
+from plone.app.vocabularies.catalog import CatalogSource
+from plone.app.z3cform.widget import RelatedItemsFieldWidget
+from plone.autoform import directives
 from plone.dexterity.content import Container
 from plone.supermodel import model
+from z3c.relationfield.schema import RelationChoice
+from z3c.relationfield.schema import RelationList
 from zope.interface import implementer
 
 
 class INewsFolder(model.Schema):
     """Marker interface and Dexterity Python Schema for NewsFolder"""
+
+    populating_newsfolders = RelationList(
+        title=_(u"Populating news folders"),
+        description=_(
+            u"News folders that populate current news folder with their events."
+        ),
+        value_type=RelationChoice(
+            title=u"Items selection",
+            source=CatalogSource(),
+        ),
+        default=[],
+        required=False,
+    )
+    directives.widget(
+        "populating_newsfolders",
+        RelatedItemsFieldWidget,
+        vocabulary="plone.app.vocabularies.Catalog",
+        pattern_options={
+            "selectableTypes": ["imio.news.NewsFolder"],
+            "favorites": [],
+        },
+    )
 
 
 @implementer(INewsFolder)
