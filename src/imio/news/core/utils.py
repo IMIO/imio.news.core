85dc52b6be71b65e9ce57f3dca823162b82717a0
Laurent Lasudry <lasudry@gmail.com>
2021-10-13 09:54:45 +0200

    Simplify code

--- src/imio/news/core/utils.py
+++ src/imio/news/core/utils.py
@@ -1,14 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from Acquisition import aq_inner
-from Acquisition import aq_parent
 from imio.news.core.contents import INewsFolder
+from Products.CMFPlone.utils import parent
 
 
 def get_news_folder_for_news_item(news_item):
     obj = news_item
     while not INewsFolder.providedBy(obj):
-        parent = aq_parent(aq_inner(obj))
-        obj = parent
+        obj = parent(obj)
     news_folder = obj
     return news_folder
